15877f3a0cd2bc77f3eb7b873e2e1a418a721f82
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-13 20:15:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,10 +24,10 @@ class School
     @roster.each do |grade, students|
       new_stu = students.sort
       sorted[grade] = []
-      sorted[grade] << new_stu
+      @rosted[grade] << new_stu
       sorted[grade].flatten
     end
-    sorted
+    @rosted
   end
   
 end
